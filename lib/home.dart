e1249164322c592d9abe71ea49c3c9972af7cc76
queonias <queones@gmail.com>
2024-02-03 12:45:41 -0300

    config: Firebase configurado

---
+++ lib/home.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('WhatsApp'),
+      ),
+      body: const Center(
+        child: Text(
+          'WhatsApp Home',
+        ),
+      ),
+    );
+  }
+}
